92e2d008fbba385df1d8e8d38833d9fb227e1d59
duc <hohuynhduca7bh123@gmail.com>
2020-12-14 23:59:54 +0700

    login and register

--- obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,112 +13,112 @@ namespace helloworld.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 1 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 2 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 3 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 4 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 5 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 6 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 7 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 8 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 9 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using helloworld;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 10 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using helloworld.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 11 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using helloworld.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 12 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using Microsoft.Extensions.Logging;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 13 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 14 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using helloworld.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 15 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 16 "c:\Users\MinhTuan\Desktop\helloworld\_Imports.razor"
+#line 16 "C:\Users\hohuy\Downloads\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
